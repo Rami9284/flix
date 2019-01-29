7de0c09698fcfa24d41887fbd8dceec903323202
Judith Ramirez <judith@Judiths-Mac.local>
2019-01-29 14:08:20 -0800

    updating project

---
+++ flix/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Judith Ramirez on 1/28/19.
+//  Copyright © 2019 Judith Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
